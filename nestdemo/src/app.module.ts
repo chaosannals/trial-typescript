2c2800a548b1b2ba3b9195d0cea8484bd29b2170
chaosannals <chaosannals@outlook.com>
2024-01-17 09:33:59 +0800

    save

--- nestdemo/src/app.module.ts
+++ nestdemo/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TalkModule } from './talk/talk.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [TalkModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api/(.*)'],
+    }),
+    TalkModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nestdemo/src/talk/talk.gateway.ts
+++ nestdemo/src/talk/talk.gateway.ts
@@ -1,11 +1,74 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import WebSocket, { Server } from 'ws';
 
-@WebSocketGateway(3000, {
-  transports: ['websocket'],
+// 指定端口时，端口不能和 HTTP 同一个。。
+// @WebSocketGateway(3001, {
+//   transports: ['websocket'],
+// })
+@WebSocketGateway({
+  // namespace: 'talk',
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+  allowEIO3: true,
+  // transports: ['websocket'],
 })
-export class TalkGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class TalkGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  afterInit() {
+    console.log('TalkGateway init.');
+  }
+
+  handleDisconnect(client: WebSocket) {
+    // client.send('close'); // 因为使用socket.io 所以不太适合使用更底层的 send，数据格式不一致。
+    console.log(`client: ${client} disconnect.`);
+  }
+  handleConnection(client: WebSocket, ...args: any[]) {
+    // client.send('open'); // 因为使用socket.io 所以不太适合使用更底层的 send，数据格式不一致。
+    console.log(
+      // Object.getPrototypeOf(client),
+      // args.map((i) => Object.getPrototypeOf(i)),
+      `client: ${client} connect. ${args.length} argc.`,
+    );
+  }
+
+  // 方法名字随意 官方示例多以 handleMessage onEvent 为例。
+  @SubscribeMessage('talk')
+  handleTalk(
+    @MessageBody() payload: string,
+    @ConnectedSocket() client: WebSocket, // 这个实例缺少很多信息（很多字段都是空的）
+  ): WsResponse<unknown> {
+    // 打印出来的字段价值都不大。
+    // console.log('handle', Object.getPrototypeOf(client), client._socket);
+    return {
+      event: 'talked', // 发送到前端的事件名。
+      data: `${client.readyState}, payload: ${payload}`,
+    };
+  }
+
+  @SubscribeMessage('say')
+  handleSay(
+    @MessageBody() payload: string,
+    @ConnectedSocket() client: WebSocket, // 这个实例缺少很多信息（很多字段都是空的）
+  ): WsResponse<unknown> {
+    return {
+      event: 'said',
+      data: `on said. ${payload}, ${client.readyState}`,
+    };
   }
 }
